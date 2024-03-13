0ecf159b5fe6b46ea61f384aa8a52970c213057e
shobana-anand <117923399+shobana-anand@users.noreply.github.com>
2024-03-13 20:02:03 +0000

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    // Other Jest configuration...
-    reporters: [
-      "default",
-      ["jest-junit", { outputDirectory: "test-results", outputName: "test-results.xml" }]
-    ]
-  };
-  
